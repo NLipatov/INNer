43f9631eae26bdb57bbd5f5694f25b8cef344e9b
Lipatov Nikita <80638093+NLipatov@users.noreply.github.com>
2021-07-26 01:37:57 +0300

    Update inn.py

--- inn.py
+++ inn.py
@@ -1,7 +1,9 @@
-import time, os, xlrd, xlwt, re, tkinter as tk
+import time, os, xlrd, xlwt, re, webbrowser, time_convert, tkinter as tk
+from tkinter import messagebox as mb
 from xlwt import easyxf
 from threading import Thread
 from selenium import webdriver
+from selenium.common.exceptions import SessionNotCreatedException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from subprocess import CREATE_NO_WINDOW
@@ -10,6 +12,7 @@ from xlutils.copy import copy
 direction = None
 workbookname = None
 status = ''
+
 
 def DirWor(Dir, Wor):
     global direction
@@ -33,9 +36,9 @@ def TT():
     os.chdir( direction )
     def Draw():
         global text, status
-        status = ''
+        status = 'Файл в работе'
         frame=tk.Frame(root,relief='solid',bd=1, bg='grey')
-        root.geometry('500x90+500+600')
+        root.geometry('700x90+500+600')
         root.iconbitmap( r"C:\INNer\INNERICC.ico" )
         root.iconbitmap( r"C:\INNer\INNERICC.ico" )
         root.title( 'Файл в работе' )
@@ -51,13 +54,14 @@ def TT():
         global text
         text.configure(text=Get_message())
         root.after(1000, Refresher)
-        if status == 'Done':
-            root.title('Готово')
+        if status == 'Готово':
+            root.title(status)
             root.lift()
             root.attributes( '-topmost', True )
             root.after_idle( root.attributes, '-topmost', False )
-            # time.sleep(10)
-            # root.destroy()
+        else:
+            root.title(status)
+
 
     root=tk.Tk()
     Draw()
@@ -66,15 +70,13 @@ def TT():
 
 
 def Main():
-    global message, status
+    global innermessage, message, status
     DriverPath = r'C:\INNer\chromedriver.exe'
-
 
     os.chdir(direction)
 
     TotalWorkingTimeStart = time.time()
     Chk1 = 0
-
 
     DriverPathExist = os.path.isfile(DriverPath)
 
@@ -82,7 +84,9 @@ def Main():
         pass
     else:
         while True:
-            print('Не обнаружен chromedriver.exe в папке с INNer\'ом')
+            status = 'Ошибка'
+            innermessage = 'Ошибка'
+            message = ('Не обнаружен chromedriver.exe в папке с INNer\'ом')
             pass
     workbook = xlrd.open_workbook(workbookname)
     sheet = workbook.sheet_by_index(0)
@@ -95,8 +99,23 @@ def Main():
     options.add_argument('--disable-gpu')
     options.add_experimental_option( 'excludeSwitches', ['enable-logging'] )
 
+    try:
+        driver = webdriver.Chrome(f'{DriverPath}', options=options)
+    except SessionNotCreatedException as session_not_created_ex:
+        error_text = str(session_not_created_ex)[29:]
+        status = 'Ошибка'
+        innermessage = 'Описание возникшей ошибки:'
+        message = ( f'\n{error_text}' )
+        if mb.askyesno(title='Требуется обновление chromedriver.exe',\
+                       message='Перейти на страницу загрузи chromedriver?'):
+            webbrowser.open('https://chromedriver.chromium.org/downloads')
+            mb.showinfo(title='Подсказка', \
+                        message='После загрузки новой версии \'chromedriver.exe\',\
+                    скопируйте её в C:\INNer с заменой файлов')
+        while True:
+            pass
 
-    driver = webdriver.Chrome(f'{DriverPath}', options=options)
+
 
 
     TotalClientsNumber = sheet.nrows - 1
@@ -120,25 +139,34 @@ def Main():
                 message += str('\n\n\nBreak. File ends here — blank cell at clients name\n\n\n')
                 break
             otch = sheet.cell_value(i,3)
+            lotch = str(sheet.cell_value(i,3)).lower()
             fam = sheet.cell_value(i,4)
             pnum = sheet.cell_value(i, 7) + sheet.cell_value(i, 8)
             bdate = sheet.cell_value(i, 19).replace('-', '')
-            if otch != '':
-                infoNowWorkingOnClient = str( f'****Работаю над: {fam} {nam[0]}. {otch[0]}.****' )
+            if lotch != '' or lotch != 'отсутствует' or lotch != 'нет' or lotch != '-':
+                middle_name = 0
             else:
-                infoNowWorkingOnClient = str( f'****Работаю над: {fam} {nam[0]}.****' )
+                middle_name = 1
+            if middle_name == 1:
+                infoNowWorkingOnClient = str( f'Приступил к: {fam} {nam[0]}. {otch[0]}.' )
+            else:
+                infoNowWorkingOnClient = str( f'Приступил к: {fam} {nam[0]}.' )
             message = message + str(infoNowWorkingOnClient)
             refreshedINN = 0
             if RFW != i:
                 Chk1 +=1
                 while True:
-                    print('Критическая ошибка: RFW != i!')
+                    status = 'Ошибка'
+                    innermessage = 'Ошибка'
+                    message = ('Критическая ошибка: RFW != i!')
                     pass
             if fam == sheet.cell_value(i,4):
                 pass
             else:
                 while True:
-                    print('\nЗавершение работы - Критическая ошибка - Фамилия из сайта и из файла не совпали')
+                    status = 'Ошибка'
+                    innermessage = 'Ошибка'
+                    message = ('\nЗавершение работы - Критическая ошибка - Фамилия из сайта и из файла не совпали')
                     pass
             if Chk1 < 1:
                     for i in range(0,10):
@@ -151,8 +179,7 @@ def Main():
                                 driver.find_element_by_name("fam").send_keys(fam[i])
                             for i in range(0, (len(nam))):
                                 driver.find_element_by_name("nam").send_keys(nam[i])
-                            if otch == '' or otch == 'Отсутствует' or otch == 'отсутствует'\
-                                    or otch == 'Нет' or otch == 'нет' or otch == '-':
+                            if lotch == '' or lotch == 'отсутствует' or lotch == 'нет' or lotch == '-':
                                 driver.find_element_by_xpath( '//*[@id="unichk_0"]' ).click()
                             else:
                                 for i in range(0, (len(otch))):
@@ -164,14 +191,17 @@ def Main():
                             driver.find_element_by_id("btn_send").click()
                             break
                         except:
-                            print('\nОшибка при загрузке данных. Проверьте работу сайта ФНС.')
+                            status = 'Ошибка'
+                            innermessage = 'Ошибка возникла до получения результата запроса, после отправки запроса.'
                     for i in range(0, 10):
                         try:
                             result = driver.find_element_by_xpath( '//*[@id="result_1"]/div' ).text
                             if result == '':
                                 time.sleep(0.5)
                                 if i == 9:
-                                    print('\nИНН не найден. Вероятно, ошибка в данных.\n')
+                                    res_msg = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/form/div[1]/div[1]/div/div[2]/p[1]').text
+                                    status = 'Ошибка'
+                                    innermessage = f'{res_msg}'
                                     TotalClientsNumberNowWorking += 1
                                     wbsheet.write(RFW, 17, '-')
                                     break
@@ -180,33 +210,47 @@ def Main():
                                 ET = time.time()
                                 print(f'\nИНН:{result[32:69]}')
                                 wb.save('INNERED — ' + workbookname)
-                                print( '\nРезультат получен и записан за %s секунды' % (ET - ST) )
+                                status = 'Файл в работе'
+                                innermessage = f'Записал — ИНН клиента {fam}: {result[32:69]}' + \
+                                               '\nРезультат получен и записан за %.1f секунды' % ((ET - ST))
                                 TotalClientsNumberNowWorking += 1
                                 break
                         except:
                             if i == 9:
-                                print(f'Не нашёл элемент, содержащий ИНН.i - {i}, fam - {fam}')
+                                status = 'Файл в работе'
+                                innermessage = 'Не нашёл элемент, содержащий ИНН'
+                                message = (f'i - {i}, fam - {fam}')
                                 wbsheet.write( RFW, 17, '-' )
                                 wb.save( 'INNERED — ' + workbookname )
                                 TotalClientsNumberNowWorking += 1
             else:
-                print('Не пройдена проверка Chk1.')
+                status = 'Критическая ошибка'
+                innermessage = 'Работа программы остановлена'
+                message = ('Не пройдена проверка Chk1.')
     except PermissionError:
-        print('Критическая ошибка — Файл нужно закрыть!')
+        status = 'Критическая ошибка'
+        innermessage = 'Работа программы остановлена'
+        message = ('Критическая ошибка — Файл нужно закрыть!')
     driver.quit()
 
     if Chk1 == 0:
-        print('\n\nРабота завершена, файл обработан.')
+        status = 'Готово!'
+        innermessage = '\nРабота завершена остановлена'
+        message = ('\nФайл обработан')
     else:
-        print('Работа завершена с ошибками!')
+        status = 'Результат работы не подлежит использованию!'
+        innermessage = 'Внимание:'
+        message = ('Работа завершена с ошибками!')
+        while True:
+            pass
     TotalWorkingTimeEnd = time.time()
-    innermessage = 'Done'
-    EndingA = ('\nПрограмма отработала за %s минут(-ы)' % ((TotalWorkingTimeEnd - TotalWorkingTimeStart) / 60))
-    EndingB = ('\nТемп равен %s секунд(-ы) на одного клиента' % (((TotalWorkingTimeEnd-TotalWorkingTimeStart)/sheet.nrows)))
+    innermessage = 'Готово'
+    EndingA = ('\nПрограмма отработала за %.1f минут(-ы)' % (((TotalWorkingTimeEnd - TotalWorkingTimeStart) / 60)))
+    EndingB = ('\nТемп равен %.1f секунд(-ы) на одного клиента' % (((TotalWorkingTimeEnd-TotalWorkingTimeStart)/sheet.nrows)))
     message = str(EndingA)
     message += str(EndingB)
     workbook.release_resources()
-    status = 'Done'
+    status = 'Работа завершена'
 
 
 
@@ -222,6 +266,8 @@ def Start():
     # Destroyer_thread.start()
     # MAin_thread.join()
     # TT_thread.join()
+
+
 
 
 
